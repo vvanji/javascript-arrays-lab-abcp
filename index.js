233b6f9e152933667ef22d7bb540cc6c6aa2736a
Evangeline VIllena <vvanjie@yahoo.com>
2018-02-13 23:16:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+kittens.push(name);
+console.log(kittens);
+}
+
+destructivelyAppendKitten("Tigger");
+
+function destructivelyPrependKitten(name){
+kittens.unshift(name);
+console.log(kittens);
+}
+
+destructivelyPrependKitten("Kit");
+
+function destructivelyRemoveLastKitten(name){
+kittens.pop();
+console.log(kittens);
+}
+
+destructivelyRemoveLastKitten();
+
+function destructivelyRemoveFirstKitten(name){
+kittens.shift();
+console.log(kittens);
+}
+
+destructivelyRemoveFirstKitten();
+
+function appendKitten(name){
+return[...kittens, name];
+}
+
+appendKitten("Tigger");
+
+function prependKitten(name){
+return [name, ...kittens];
+}
+
+prependKitten("Tigger");
+
+function removeLastKitten(){
+return kittens.slice(0, kittens.length -1);
+}
+
+removeLastKitten();
+
+function removeFirstKitten(){
+return kittens.slice(1);
+}
+
+removeFirstKitten();
+
