6d5850550034518c07cd29d9425b5f96c1ab1718
Evangeline VIllena <vvanjie@yahoo.com>
2018-02-13 23:16:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ return [name, ...kittens];
 prependKitten("Tigger");
 
 function removeLastKitten(){
-return kittens.slice( );
+return kittens.slice(0, kittens.length -1 );
 }
 
 removeLastKitten();
